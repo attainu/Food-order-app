c63c92e862edfbe7f41fa70a567b2d0cb26756ad
Swati Shelke <ghogareswati14@gmail.com>
2020-08-16 16:29:57 +0530

    Recipe pagination

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,18 +1,34 @@
-import React, { Component } from 'react'
-import "../styles/RestaurantDetails.css"
- class Review extends Component {
-    render() {
-        return (
-            <div className="review">
-                 <p><img src={`${this.props.rev.review.user.profile_image}`} alt="nopic"/></p>
-                 <p>Name:{this.props.rev.review.user.name}</p>
-                <p>Rating:{this.props.rev.review.rating}</p>
-                <p>Review:{this.props.rev.review.review_text}</p>
-                <p>Short Review:{this.props.rev.review.rating_text}</p>
-                <p>Time:{this.props.rev.review.review_time_friendly}</p>
-            </div>
-        )
-    }
+import React, { Component } from "react";
+import "../styles/RestaurantDetails.css";
+class Review extends Component {
+  render() {
+    return (
+      <div className="review">
+        <p>
+          <img
+            src={`${this.props.rev.review.user.profile_image}`}
+            alt="nopic"
+          />
+        </p>
+        <p>
+          Name:<span>{this.props.rev.review.user.name}</span>
+        </p>
+        <p>
+          Rating:<span>{this.props.rev.review.rating}</span>
+        </p>
+        <p>
+          Review:<span>{this.props.rev.review.review_text}</span>
+        </p>
+        <p>
+          Short Review:<span>{this.props.rev.review.rating_text}</span>
+        </p>
+        <p>
+          Time:<span>{this.props.rev.review.review_time_friendly}</span>
+        </p>
+        <hr />
+      </div>
+    );
+  }
 }
 
-export default Review
+export default Review;
--- src/redux/reducers/foodreducer.js
+++ src/redux/reducers/foodreducer.js
@@ -1,16 +1,21 @@
-import { SET_FOOD, SET_FOODLOADING } from "../actionname"
+import { SET_FOOD, SET_FOODLOADING, GET_PAGE } from "../actionname";
 
 const initialState = {
-    foods: [],
-    foodLoading: false,
-}
+  foods: [],
+  foodLoading: false,
+  page: 0,
+};
 
 export const foodReducer = (state = initialState, action) => {
-    const { type, payload } = action
-    switch (type) {
-        case SET_FOOD: return { ...state, foods: payload }
-        case SET_FOODLOADING: return { ...state, foodLoading: !state.foodLoading }
-        default: return state
-    }
-}
-
+  const { type, payload } = action;
+  switch (type) {
+    case SET_FOOD:
+      return { ...state, foods: payload };
+    case SET_FOODLOADING:
+      return { ...state, foodLoading: !state.foodLoading };
+    case GET_PAGE:
+      return { ...state, page: payload };
+    default:
+      return state;
+  }
+};
